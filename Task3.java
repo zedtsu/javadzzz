fcd6ef2401187fb2624b60d0a3c7724c96a93487
Alexandr <you@example.com>
2023-07-30 15:24:58 +0800

    asfasf

--- Task3.java
+++ Task3.java
@@ -6,6 +6,7 @@ public class Task3 {
             System.out.println(a / b);
             printSum(23, 234);
             int[] abc = { 1, 2 };
+            abc[3] = 9;
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("Массив выходит за пределы своего размера!");
         }
